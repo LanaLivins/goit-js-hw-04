6a8189f55cc3bb763330142271ef7a4c44deef00
Svitlana Shevkoplias <shevkopliassvitlana@gmail.com>
2024-12-19 14:26:50 +0100

    hm-4

---
+++ JS/task-2.js
@@ -0,0 +1,27 @@
+function calcAverageCalories(days) {
+  if (days.length === 0) {
+    return 0;
+  }
+
+  let totalCalories = 0;
+  for (let day of days) {
+    totalCalories += day.calories;
+  }
+
+  const averageCalories = totalCalories / days.length;
+  return averageCalories;
+}
+
+console.log(
+  calcAverageCalories([
+    { day: 'monday', calories: 2040 },
+    { day: 'tuesday', calories: 2270 },
+    { day: 'wednesday', calories: 2420 },
+    { day: 'thursday', calories: 1900 },
+    { day: 'friday', calories: 2370 },
+    { day: 'saturday', calories: 2280 },
+    { day: 'sunday', calories: 2610 },
+  ])
+); // 2270
+
+console.log(calcAverageCalories([])); // 0

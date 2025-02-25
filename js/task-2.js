1881d63060b30d196e1857a55ebbd0ccfcf6c73d
Svitlana Shevkoplias <shevkopliassvitlana@gmail.com>
2025-02-25 22:12:23 +0100

    update-hm

--- js/task-2.js
+++ js/task-2.js
@@ -1,13 +1,16 @@
 function calcAverageCalories(days) {
-  let totalCalories = 0;
+  if (days.length === 0) {
+    return 0;
+  } else {
+    let totalCalories = 0;
 
-  for (let day of days) {
-    totalCalories += day.calories;
+    for (let day of days) {
+      totalCalories += day.calories;
+    }
+
+    const averageCalories = totalCalories / days.length;
+    return averageCalories;
   }
-
-  const averageCalories = totalCalories / days.length;
-
-  return averageCalories;
 }
 
 console.log(
